aac79c1154d1ff3a5f0c6c256ae743b276244db4
arturtigranyan <arttigranyan@gmail.com>
2018-03-06 01:17:50 -0800

    Payout factor

--- contracts/PonziGame.sol
+++ contracts/PonziGame.sol
@@ -5,24 +5,33 @@ contract ponziGame {
     struct Investment {
         address investor;
         uint amount;
+        uint payout;
     }
 
     address private boss;
     
     uint top = 0;
     
+    uint totalInvestment;
+    
+    uint128 payoutFactor_;
+    
     Investment[] investments;
+    
+    function ponziGame(uint128 payoutFactor) public {
+        payoutFactor_ = payoutFactor;
+    }
 
     function invest() payable {
         
-        require(msg.value => 100);
+        require(msg.value >= 100);
             
-        investments.push(Investment(msg.sender, msg.value));
+        investments.push(Investment(msg.sender, msg.value, msg.value * payoutFactor_ / 100 ));
         
         boss.transfer(msg.value * 5 / 100);
         
-        while(investments[top].amount * 110 / 100  < this.balance){
-            investments[top].investor.transfer(investments[top].amount * 110 / 100);
+        while(investments[top].amount < this.balance){
+            investments[top].investor.transfer(investments[top].amount);
             top++;
         }
     }
@@ -30,5 +39,4 @@ contract ponziGame {
      function getEthBalance() constant returns(uint) {
         return this.balance;
     }
-
 }
