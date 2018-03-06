9216d14abf692d85f322b3aac91e4bdd7f728b39
arturtigranyan <arttigranyan@gmail.com>
2018-03-06 05:40:15 -0800

    Payout factor (#3)
    * Payout factor
    
    * Bug fixing
    
    * Setting peyout
    
    * Fixing a bug
    
    * removing unused variable

--- contracts/PonziGame.sol
+++ contracts/PonziGame.sol
@@ -5,24 +5,34 @@ contract ponziGame {
     struct Investment {
         address investor;
         uint amount;
+        uint payoutFactor;
     }
 
-    address private boss;
-    
-    uint top = 0;
+    address private boss;    
+    uint top = 0;    
+    uint8 payoutFactor_;
     
     Investment[] investments;
+    
+    function ponziGame(uint128 payoutFactor) public {
+        payoutFactor_ = payoutFactor;
+    }
+    
+    function setPayoutFactor(uint128 newPayout) public {
+        require(newPayout >= 110 && newPayout <= 130);
+        payoutFactor_ = newPayout;
+    }
 
     function invest() payable {
         
-        require(msg.value => 100);
-            
-        investments.push(Investment(msg.sender, msg.value));
+        require(msg.value >= 100);
+        
+        investments.push(Investment(msg.sender, msg.value, payoutFactor_));
         
         boss.transfer(msg.value * 5 / 100);
         
-        while(investments[top].amount * 110 / 100  < this.balance){
-            investments[top].investor.transfer(investments[top].amount * 110 / 100);
+        while(investments[top].amount * investments[top].payoutFactor / 100 < this.balance){
+            investments[top].investor.transfer(investments[top].amount * investments[top].payoutFactor / 100);
             top++;
         }
     }
@@ -30,5 +40,4 @@ contract ponziGame {
      function getEthBalance() constant returns(uint) {
         return this.balance;
     }
-
 }
