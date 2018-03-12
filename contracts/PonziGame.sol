0638fa83e6164b701dc480eb8c61aa6d1d57990b
arturtigranyan <arttigranyan@gmail.com>
2018-03-12 08:49:28 -0700

    making initial changes

--- contracts/PonziGame.sol
+++ contracts/PonziGame.sol
@@ -7,24 +7,27 @@ contract ponziGame {
         uint amount;
         uint payoutFactor;
     }
-
+    
     address private boss;
-    address[] investors;
+    address[] vipInvestors_;
+    
     
     uint top = 0;
     
-    uint128 payoutFactor_;
+    uint8 payoutFactor_;
     
     Investment[] investments;
     
     mapping(address => uint) public investorInvestment;
     
-    function ponziGame(uint128 payoutFactor) public {
+    function ponziGame(uint8 payoutFactor, uint8 vipInvestors) public {
         
         payoutFactor_ = payoutFactor;
+        
+        vipInvestors_.length = vipInvestors; 
     }
     
-    function setPayoutFactor(uint128 newPayout) public {
+    function setPayoutFactor(uint8 newPayout) public {
         require(newPayout >= 110 && newPayout <= 130);
         payoutFactor_ = newPayout;
     }
@@ -45,12 +48,28 @@ contract ponziGame {
         }
     }
     
-    function investorsRankedByInvesment(uint8 index) public constant returns (address investor, uint totalInvested){
+    function getTotalInvestments(uint8 index) public constant returns (address investor, uint totalInvested){
         investor = investments[index].investor;
         totalInvested = investorInvestment[investor];
     }
     
-     function getEthBalance() constant returns(uint) {
+    function getVipInvestors(uint8 index) public constant returns (address vipInvestor, uint totalInvested){
+        require(0 <= index && index < vipInvestors_.length);
+        vipInvestor = investments[index].investor;
+        totalInvested = investorInvestment[vipInvestor];
+    }
+    
+    function updateVipInvestor(address investor) public {
+        uint8 insert = 0;
+        uint newInvestment = investorInvestment[investor];
+        
+        if(insert < vipInvestors_.length){
+            vipInvestors_[insert] = investor;
+        }
+        
+    }
+    
+    function getEthBalance() constant returns(uint) {
         return this.balance;
     }
 }
